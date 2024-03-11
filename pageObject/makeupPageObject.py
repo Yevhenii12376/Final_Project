aedcb963beadb3fb89f07adf9394d182fe440794
Yevhenii12376 <48484535+Yevhenii12376@users.noreply.github.com>
2024-03-11 19:02:17 +0200

    Final_Projects

--- pageObject/makeupPageObject.py
+++ pageObject/makeupPageObject.py
@@ -6,20 +6,28 @@ class MakeUp:
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
 
-    def open_url(self, url:str):
+    def open_url(self, url: str):
         self.page.goto(url)
-        return(self)
+        return self
 
-    def test_Autorization(self):
+    def field_email(self, email: str):
+        self.page.get_by_role("textbox", name="E-mail").click()
+        self.page.get_by_role("textbox", name="E-mail").fill(email)
+        return self
+
+    def field_password(self, password: str):
+        self.page.get_by_placeholder("Пароль").fill(password)
+        return self
+
+    def Autorization(self):
         self.page.goto("https://makeup.com.ua/ua/")
         self.page.locator(".header-office").click()
-        self.page.get_by_label("E-mail").click()
-        self.page.get_by_label("E-mail").fill("o.ionenko129@gmail.com")
+        self.field_email("o.ionenko129@gmail.com")
         self.page.get_by_label("E-mail").press("Tab")
-        self.page.get_by_placeholder("Пароль").fill("13052000n")
+        self.field_password("13052000n")
         self.page.get_by_placeholder("Пароль").press("Enter")
 
-    def test_checking_pages(self):
+    def checking_pages(self):
         self.page.get_by_role("link", name="Парфумерія", exact=True).click()
         self.page.goto("https://makeup.com.ua/ua/product/977/")
         self.page.get_by_title("В обране").click()
@@ -31,7 +39,7 @@ class MakeUp:
         self.page.get_by_text("Оформити замовлення").click()
         self.page.get_by_role("link", name="MAKEUP").click()
 
-    def test_checking_others_pages(self):
+    def checking_others_pages(self):
         # Перевірка сторінки Доставка та оплата
         self.page.get_by_role("link", name="Доставка та Оплата").click()
         # Перевірка сторінки Про нас
@@ -45,7 +53,7 @@ class MakeUp:
         self.page.locator(".header-office").click()
         self.page.get_by_role("link", name="Вихід").click()
 
-    def test_product_search(self):
+    def product_search(self):
         self.page.goto("https://makeup.com.ua/ua/")
         # Перевірка кнопки пошуку товару
         self.page.locator(".search-button").first.click()
@@ -54,7 +62,7 @@ class MakeUp:
         self.page.get_by_placeholder("Понад 249 000 товарів").press("Enter")
         self.page.goto("https://makeup.com.ua/ua/product/32739/")
 
-    def test_filter_check(self):
+    def filter_check(self):
         self.page.goto("https://makeup.com.ua/ua/")
         # Перехід на сторінку Тіло і ванна
         self.page.get_by_role("link", name="Тіло і ванна").click()
@@ -62,28 +70,28 @@ class MakeUp:
         # Перехід на товар
         self.page.goto("https://makeup.com.ua/ua/categorys/321073/#o[2243][]=29953")
 
-    def test_password_change_verification(self):
+    def password_change_verification(self):
         self.page.goto("https://makeup.com.ua/ua/")
         self.page.locator(".header-office").click()
         self.page.get_by_text("Забули пароль?").click()
         self.page.wait_for_timeout(1500)
-        self.page.get_by_role("textbox", name="E-mail").click()
-        self.page.get_by_role("textbox", name="E-mail").fill("o.ionenko129@gmail.com")
+        self.field_email("o.ionenko129@gmail.com")
         self.page.get_by_role("button", name="Нагадати").click()
         self.page.locator("#popup__window div").first.click()
 
-    def test_email_change_verification(self):
+    def email_change_verification(self):
         self.page.goto("https://makeup.com.ua/ua/")
         self.page.locator(".header-office").click()
         self.page.get_by_label("E-mail").fill("o.ionenko129@gmail.com")
         self.page.get_by_role("button", name="Увійти").click()
 
-    def test_verification_without_login(self):
+    def verification_without_login(self):
         self.page.goto("https://makeup.com.ua/ua/")
         self.page.locator(".header-office").click()
         self.page.get_by_placeholder("Пароль").click()
-        self.page.get_by_placeholder("Пароль").fill("13052000n")
+        self.field_password("13052000n")
         self.page.get_by_role("button", name="Увійти").click()
+
     def close(self):
         self.page.close()
         self.context.close()
