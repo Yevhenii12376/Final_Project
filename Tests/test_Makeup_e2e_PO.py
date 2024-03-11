95044439c003868069895ad3dd7753b4b46016dc
Yevhenii12376 <48484535+Yevhenii12376@users.noreply.github.com>
2024-03-11 19:02:49 +0200

    Final_Projects

--- Tests/test_Makeup_e2e_PO.py
+++ Tests/test_Makeup_e2e_PO.py
@@ -1,12 +1,20 @@
 def test_Makeup_e2e(desktop_makeup):
     desktop_makeup.open_url("https://makeup.com.ua/ua/")
-    desktop_makeup.test_Autorization()
-    desktop_makeup.test_checking_pages()
-    desktop_makeup.test_checking_others_pages()
+    desktop_makeup.Autorization()
+    desktop_makeup.checking_pages()
+    desktop_makeup.checking_others_pages()
 
 def test_Makeup_negative(desktop_makeup):
-    desktop_makeup.test_product_search()
-    desktop_makeup.test_filter_check()
-    desktop_makeup.test_password_change_verification()
-    desktop_makeup.test_email_change_verification()
-    desktop_makeup.test_verification_without_login()
+    desktop_makeup.product_search()
+
+def test_Makeup_negative2(desktop_makeup):
+    desktop_makeup.filter_check()
+
+def test_Makeup_negative3(desktop_makeup):
+    desktop_makeup.password_change_verification()
+
+def test_Makeup_negative4(desktop_makeup):
+    desktop_makeup.email_change_verification()
+
+def test_Makeup_negative5(desktop_makeup):
+    desktop_makeup.verification_without_login()
